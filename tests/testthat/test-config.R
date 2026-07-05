9490e967e64318c96b4d1e97b7b6cf1e09a94b45
James Balamuta <james.balamuta@gmail.com>
2026-07-05 17:43:39 -0500

    Enable the older marutter c2d4u3.5 and c2d4u archives

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -2,7 +2,8 @@ test_that("config exposes the archive registry and column contract", {
   expect_true(is.list(ARCHIVES) && length(ARCHIVES) == 3)
   expect_identical(ARCHIVES[[1]]$key, "c2d4u4.0+")
   expect_true(ARCHIVES[[1]]$enabled)
-  expect_false(ARCHIVES[[2]]$enabled)
+  expect_identical(ARCHIVES[[2]]$key, "c2d4u3.5")
+  expect_identical(ARCHIVES[[3]]$key, "c2d4u")
   expect_identical(SUMMARY_COLS[1:4],
                    c("package", "package_lower", "origin", "canonical_name"))
   expect_true("cnt_total" %in% SUMMARY_COLS)
